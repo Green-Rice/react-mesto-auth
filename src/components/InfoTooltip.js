db28ba7de1748f2b5b7830a1c4855c1d02dc0ebb
Green rice <alexeyerokhin_ck@mail.ru>
2023-06-18 22:09:01 +0300

    1,2

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,11 +1,14 @@
 import React from "react";
+import advance from '../images/tick.png'
+import fiasco from '../images/cross.png';
+
 
 function InfoTooltip() {
 
 
     return (
         <div className="">
-            <div className="">
+            <div className="popup__container">
                 <button type="button" className="popup__close" ></button>
                 <img className=""/>
                 <h2 className=""></h2>
